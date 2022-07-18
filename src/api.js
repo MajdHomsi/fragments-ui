7681e39488a8b8595a3b26eb24fa392cb7f68ab8
MajdHomsi <majd.homsi.ca@gmail.com>
2022-07-17 22:55:18 -0400

    A2 front end addition

--- src/api.js
+++ src/api.js
@@ -1,48 +1,118 @@
 // src/api.js
-
-// fragments microservice API, defaults to localhost:8080
-const apiUrl = process.env.API_URL || 'http://localhost:8080';
-
-/**
- * Given an authenticated user, request all fragments for this user from the
- * fragments microservice (currently only running locally). We expect a user
- * to have an `idToken` attached, so we can send that along with the request.
- */
+const apiUrl = process.env.API_URL;
 export async function getUserFragments(user) {
   console.log('Requesting user fragments data...');
   try {
     const res = await fetch(`${apiUrl}/v1/fragments`, {
-      // Generate headers with the proper Authorization bearer token to pass
-      
-      headers: user.authorizationHeaders(),
+      headers: {
+        Authorization: `Bearer ${user.idToken}`,
+      },
     });
     if (!res.ok) {
       throw new Error(`${res.status} ${res.statusText}`);
     }
     const data = await res.json();
-    console.log('Got user fragments data', { data });
-    return data;
+    console.log( data );
+
   } catch (err) {
     console.error('Unable to call GET /v1/fragment', { err });
   }
-  
 }
 
-  export async function postUserFragments(user, value) {
-    console.log('Posting fragment...');
-    try {
-      const res = await fetch(`${apiUrl}/v1/fragments`, {
-        method: 'POST',
-        // Generate headers with the proper Authorization bearer token to pass
-        headers: user.authorizationHeaders(),body:value
-       
-      });
-      if (!res.ok) {
-        throw new Error(`${res.status} ${res.statusText}`); 
-      }
-      const data = await res.json();
-      console.log('Got user fragments data', { data });
-    } catch (err) {
-      console.error('Unable to call POST /v1/fragment', { err });
+export async function postUserFragments(user,frag,type) {
+  console.log('Posting user fragments data...');
+  try {
+    if(type == 'application/json'){
+      frag = JSON.parse(JSON.stringify(frag));
+      console.log(frag);
     }
+    const res = await fetch(`${apiUrl}/v1/fragments`, {
+      method: "post",
+      headers: {
+        Authorization: `Bearer ${user.idToken}`,
+        'Content-type': type,
+      },
+      body: frag
+    });
+
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    
+    console.log('Posted user fragments data', { data });
+  } catch (err) {
+    console.error('Unable to call POST /v1/fragment', { err });
+  }
 }
+export async function getUserFragmentList(user) {
+  console.log('Requesting user fragments data...');
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/?expand=1`, {
+      headers: {
+        Authorization: `Bearer ${user.idToken}`,
+      },
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    console.log( data );
+
+  } catch (err) {
+    console.error('Unable to call GET /v1/fragment/?expand=1', { err });
+  }
+}
+
+export async function getFragmentDataById(user,id) {
+  try {
+    if(id != ""){
+    console.log(`Requesting user fragments data by id...${id}`);
+    console.log(`fetching ${apiUrl}/v1/fragments/${id}`);
+    const res = await fetch(`${apiUrl}/v1/fragments/${id}` , {
+      headers: {
+        Authorization: `Bearer ${user.idToken}`,
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const type = res.headers.get("Content-Type");
+    if(type.includes("text")){
+    const data = await res.text();
+    console.log(`Got user fragment by id ${id}:` + data);
+    document.getElementById("dataBack").innerHTML = data;
+    }
+    if(type.includes("json")){
+      const data = await res.json();
+      console.log(data);
+    }
+    }else{
+      document.getElementById("dataBack").textContent = "id can not be empty";
+    }
+  } catch (err) {
+    console.error(`Unable to call GET /v1/fragment/ ${id}`, { err });
+  }
+
+}
+
+export async function getFragmentInfo(user,id) {
+  console.log(`Requesting user fragments info by id...${id}`);
+  console.log(`fetching ${apiUrl}/v1/fragments/${id}/info`);
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${id}/info` , {
+      headers: {
+        Authorization: `Bearer ${user.idToken}`,
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    console.log(data);
+  } catch (err) {
+    console.error(`Unable to call GET /v1/fragments/${id}/info`, { err });
+  }
+} 
--- src/app.js
+++ src/app.js
@@ -1,74 +1,62 @@
 // src/app.js
-
 import { Auth, getUser } from './auth';
-import { getUserFragments, postUserFragments } from './api';
+import { getUserFragmentList, getUserFragments, postUserFragments, getFragmentDataById, getFragmentInfo } from './api';
 
 async function init() {
-  // Get our UI elements
   const userSection = document.querySelector('#user');
-  const fragmentSection= document.querySelector('#fragment-section');
-  const getIdBtn = document.querySelector('#getIdBtn');
-  const postBtn = document.querySelector('#postBtn');
   const loginBtn = document.querySelector('#login');
   const logoutBtn = document.querySelector('#logout');
-  
+  const postSection = document.querySelector('#post');
+  const postBTN = document.querySelector('#postBtn');
+  const getBTN = document.querySelector('#getBtn');
+  const getListBTN = document.querySelector('#getListBtn');
+  const getByIdBTN = document.querySelector('#getByIdBtn');
+  const getInfoBTN = document.querySelector('#getInfoBtn');
 
-  // Wire up event handlers to deal with login and logout.
   loginBtn.onclick = () => {
-    // Sign-in via the Amazon Cognito Hosted UI (requires redirects), see:
-    // https://docs.amplify.aws/lib/auth/advanced/q/platform/js/#identity-pool-federation
     Auth.federatedSignIn();
   };
   logoutBtn.onclick = () => {
-    // Sign-out of the Amazon Cognito Hosted UI (requires redirects), see:
-    // https://docs.amplify.aws/lib/auth/emailpassword/q/platform/js/#sign-out
     Auth.signOut();
   };
 
-  //console.log(postBtn);
- 
-  // See if we're signed in (i.e., we'll have a `user` object)
   const user = await getUser();
   if (!user) {
-    // Disable the Logout button
     logoutBtn.disabled = true;
-    fragmentSection.hidden=true;
     return;
   }
-    else{
-      fragmentSection.hidden=false;
-    }
 
-    postBtn.onclick = () => {
-      const fragmentData = document.querySelector('#inputFragment').value;
-      postUserFragments(user, fragmentData);
-    }
-  
-    getIdBtn.onclick = async() => {
-      const id = document.querySelector('#outputFragment').value;
-      const fragments = await getUserFragments(user);
-      console.log(fragments);
-    }
-  
-  
-    // Do an authenticated request to the fragments API server and log the result
+  postBTN.onclick = () => {
+    let data = document.querySelector('#data').value;
+    let type = document.querySelector('#types').value;
+    console.log(type);
+    postUserFragments(user,data,type);
+
+  }
+  getBTN.onclick = () => {
     getUserFragments(user);
+  }
+  getListBTN.onclick = () => {
+    getUserFragmentList(user);
+  }
 
-  // Log the user info for debugging purposes
+  getByIdBTN.onclick = () => {
+    let id = document.querySelector('#id').value
+    getFragmentDataById(user,id);
+  }
+  getInfoBTN.onclick = () => {
+  let id = document.querySelector('#id').value
+  getFragmentInfo(user,id);
+ }
   console.log({ user });
-
-  // Update the UI to welcome the user
   userSection.hidden = false;
+  userSection.querySelector('.username').innerHTML = user.username;
 
-  // Show the user's username
-  userSection.querySelector('.username').innerText = user.username;
-
-  // Disable the Login button
+  loginBtn.disabled = true;
   if(loginBtn.disabled = true){
-   fragmentSection.disabled = false
-  };
+    postSection.hidden = false;
+  }
+  
 
 }
-
-// Wait for the DOM to be ready, then start the app
 addEventListener('DOMContentLoaded', init);
